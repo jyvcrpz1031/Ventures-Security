bce1728857d9319bffb0cc61790a49186763e53b
Jayvee Corpuz <corpuzjayvee31@gmail.com>
2022-05-13 17:24:00 +0800

    move isolate to app

--- src/components/page/Home/index.js
+++ src/components/page/Home/index.js
@@ -15,7 +15,7 @@ class Home extends React.Component {
     render() {
         return (
             <div className="home-section h-100 w-full relative">
-                <div className="content isolate z-[4] h-full w-full flex h-[845px] md:h-[1000px] justify-center items-center">
+                <div className="content z-[4] h-full w-full flex h-[845px] md:h-[1000px] justify-center items-center">
                     <div className="header z-[1] text-center">
                         <h2 className="text-5xl md:text-6xl">{this.props.header}</h2>
                         <Text className="venture-adv mt-10 md:text-xl" text={this.props.infoText} />
