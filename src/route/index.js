a08d46af805376db720ac29cb96273000ce63819
Jayvee Corpuz <corpuzjayvee31@gmail.com>
2022-05-13 16:50:57 +0800

    add isolate on main-content

--- src/route/index.js
+++ src/route/index.js
@@ -69,7 +69,7 @@ class RenderPages extends React.Component {
 
     render = () => {
         return (
-            <div className="main-content h-full w-full bg-no-repeat bg-cover bg-fixed bg-center">
+            <div className="main-content h-full w-full bg-no-repeat bg-cover bg-fixed bg-center isolate">
                 {this.renderPage()}
             </div>
         );
