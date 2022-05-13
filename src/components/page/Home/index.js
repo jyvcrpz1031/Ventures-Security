18c769b39c3f34447b340804a5d343a82b3a1f65
Jayvee Corpuz <corpuzjayvee31@gmail.com>
2022-05-13 18:07:53 +0800

    revert changes

--- src/components/page/Home/index.js
+++ src/components/page/Home/index.js
@@ -16,7 +16,6 @@ class Home extends React.Component {
         return (
             <div className="home-section h-100 w-full relative">
                 <div className="content z-[4] h-full w-full flex h-[845px] md:h-[1000px] justify-center items-center">
-                    <div className="content-bg mix-blend-hard-light absolute"></div>
                     <div className="header z-[1] text-center">
                         <h2 className="text-5xl md:text-6xl">{this.props.header}</h2>
                         <Text className="venture-adv mt-10 md:text-xl" text={this.props.infoText} />
